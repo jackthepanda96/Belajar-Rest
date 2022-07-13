42a9de2aaf73178b1dae9f12aa307cc73f4f3b1b
Jerry <jerryyoung2911@gmail.com>
2022-07-13 11:20:00 +0700

    Implement GORM

--- go.mod
+++ go.mod
@@ -2,7 +2,13 @@ module github.com/jackthepanda96/Belajar-Rest.git
 
 go 1.18
 
+require gorm.io/gorm v1.23.8
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
+
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo/v4 v4.7.2 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
@@ -13,4 +19,5 @@ require (
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/mysql v1.3.5
 )
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -17,161 +19,169 @@ type User struct {
 }
 
 var (
-	arrData []User
+	dataNumber int
+	// DB         *gorm.DB
 )
 
-func cariData(id int) (int, bool) {
-	for idx, val := range arrData {
-		if val.ID == id {
-			return idx, true
+func InitDB() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", "root", "", "localhost", 3307, "echorm")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Cannot connect to DB")
+	}
+
+	db.AutoMigrate(User{})
+	return db
+}
+
+func GetAll(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var tmp []User
+		err := db.Find(&tmp).Error
+
+		if err != nil {
+			log.Println("Cannot retrive object", err.Error())
+			return c.JSON(http.StatusInternalServerError, "Error dari server")
 		}
-	}
 
-	return -1, false
+		res := map[string]interface{}{
+			"message": "Get all data",
+			"data":    tmp,
+		}
+		return c.JSON(http.StatusOK, res)
+	}
 }
 
-func GetAll(c echo.Context) error {
-	res := map[string]interface{}{
-		"message": "Get all data",
-		"data":    arrData,
+func GetSpecificUser(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var tmp User
+
+		param := c.Param("id")
+		cnv, err := strconv.Atoi(param)
+		if err != nil {
+			log.Println("Cannot convert to int", err.Error())
+			return c.JSON(http.StatusInternalServerError, "cannot convert id")
+		}
+
+		err = db.Where("ID = ?", cnv).First(&tmp).Error
+		if err != nil {
+			log.Println("There is a problem with data", err.Error())
+			return c.JSON(http.StatusBadRequest, "no data")
+		}
+
+		res := map[string]interface{}{
+			"message": "Get all data",
+			"data":    tmp,
+		}
+		return c.JSON(http.StatusOK, res)
 	}
-	return c.JSON(http.StatusOK, res)
 }
 
-func GetSpecificUser(c echo.Context) error {
-	param := c.Param("id")
-	cnv, err := strconv.Atoi(param)
-	if err != nil {
-		log.Println("Cannot convert to int", err.Error())
-		return c.JSON(http.StatusInternalServerError, "cannot convert id")
-	}
+func InsertUser(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var tmp User
+		err := c.Bind(&tmp)
+		if err != nil {
+			log.Println("Cannot parse input to object", err.Error())
+			return c.JSON(http.StatusInternalServerError, "Error dari server")
+		}
 
-	if cnv > len(arrData) {
-		log.Println("Index out of range")
-		return c.JSON(http.StatusInternalServerError, "Index out of range")
-	}
+		if dataNumber == 0 {
+			tmp.ID = 1
+		} else {
+			dataNumber++
+			tmp.ID = dataNumber
+		}
 
-	idxData, isFound := cariData(cnv)
+		err = db.Create(&tmp).Error
+		if err != nil {
+			log.Println("Cannot create object", err.Error())
+			return c.JSON(http.StatusInternalServerError, "Error dari server")
+		}
 
-	if !isFound {
-		log.Println("Data not found")
-		return c.JSON(http.StatusNotFound, "Data not found")
+		res := map[string]interface{}{
+			"message": "Success input data",
+			"data":    tmp,
+		}
+		return c.JSON(http.StatusOK, res)
 	}
-
-	res := map[string]interface{}{
-		"message": "Get all data",
-		"data":    arrData[idxData],
-	}
-	return c.JSON(http.StatusOK, res)
 }
 
-func InsertUser(c echo.Context) error {
-	var tmp User
-	err := c.Bind(&tmp)
-	if err != nil {
-		log.Println("Cannot parse input to object", err.Error())
-		return c.JSON(http.StatusInternalServerError, "Error dari server")
-	}
+func UpdateUser(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		qry := map[string]interface{}{}
+		param := c.Param("id")
+		cnv, err := strconv.Atoi(param)
+		if err != nil {
+			log.Println("Cannot convert to int", err.Error())
+			return c.JSON(http.StatusInternalServerError, "cannot convert id")
+		}
 
-	if len(arrData) == 0 {
-		tmp.ID = 1
-	} else {
-		tmp.ID = arrData[len(arrData)-1].ID + 1
-	}
+		var tmp User
+		err = c.Bind(&tmp)
+		if err != nil {
+			log.Println("Cannot parse input to object", err.Error())
+			return c.JSON(http.StatusInternalServerError, "Error dari server")
+		}
 
-	arrData = append(arrData, tmp)
-	res := map[string]interface{}{
-		"message": "Success input data",
-		"data":    tmp,
+		if tmp.Nama != "" {
+			qry["nama"] = tmp.Nama
+		}
+
+		if tmp.Email != "" {
+			qry["email"] = tmp.Email
+		}
+
+		if tmp.Password != "" {
+			qry["password"] = tmp.Password
+		}
+		var ret User
+		err = db.Model(&ret).Where("ID = ?", cnv).Updates(qry).Error
+		if err != nil {
+			log.Println("Cannot update data", err.Error())
+			return c.JSON(http.StatusInternalServerError, "cannot update")
+		}
+
+		res := map[string]interface{}{
+			"message": "Success update data",
+			"data":    ret,
+		}
+
+		return c.JSON(http.StatusOK, res)
 	}
-	return c.JSON(http.StatusOK, res)
 }
 
-func UpdateUser(c echo.Context) error {
-	param := c.Param("id")
-	cnv, err := strconv.Atoi(param)
-	if err != nil {
-		log.Println("Cannot convert to int", err.Error())
-		return c.JSON(http.StatusInternalServerError, "cannot convert id")
+func DeleteUser(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		param := c.Param("id")
+		cnv, err := strconv.Atoi(param)
+		if err != nil {
+			log.Println("Cannot convert to int", err.Error())
+			return c.JSON(http.StatusInternalServerError, "cannot convert id")
+		}
+
+		err = db.Where("ID = ?", cnv).Delete(&User{}).Error
+		if err != nil {
+			log.Println("Cannot delete data", err.Error())
+			return c.JSON(http.StatusInternalServerError, "cannot delete")
+		}
+
+		res := map[string]interface{}{
+			"message": "Success delete data",
+		}
+		return c.JSON(http.StatusOK, res)
 	}
-
-	if cnv > len(arrData) {
-		log.Println("Index out of range")
-		return c.JSON(http.StatusInternalServerError, "Index out of range")
-	}
-
-	var tmp User
-	err = c.Bind(&tmp)
-	if err != nil {
-		log.Println("Cannot parse input to object", err.Error())
-		return c.JSON(http.StatusInternalServerError, "Error dari server")
-	}
-
-	idxData, isFound := cariData(cnv)
-
-	if !isFound {
-		log.Println("Data not found")
-		return c.JSON(http.StatusNotFound, "Data not found")
-	}
-	// arrData[idxData] = tmp --> update smua
-	if tmp.Nama != "" {
-		arrData[idxData].Nama = tmp.Nama
-	}
-
-	if tmp.Email != "" {
-		arrData[idxData].Email = tmp.Email
-	}
-
-	if tmp.Password != "" {
-		arrData[idxData].Password = tmp.Password
-	}
-
-	res := map[string]interface{}{
-		"message": "Success update data",
-		"data":    arrData[idxData],
-	}
-
-	return c.JSON(http.StatusOK, res)
-}
-
-func DeleteUser(c echo.Context) error {
-	param := c.Param("id")
-	cnv, err := strconv.Atoi(param)
-	if err != nil {
-		log.Println("Cannot convert to int", err.Error())
-		return c.JSON(http.StatusInternalServerError, "cannot convert id")
-	}
-
-	if cnv > len(arrData) {
-		log.Println("Index out of range")
-		return c.JSON(http.StatusInternalServerError, "Index out of range")
-	}
-
-	idxData, isFound := cariData(cnv)
-
-	if !isFound {
-		log.Println("Data not found")
-		return c.JSON(http.StatusNotFound, "Data not found")
-	}
-
-	left := arrData[:idxData]
-	right := arrData[idxData+1:]
-	arrData = left
-	arrData = append(arrData, right...)
-
-	res := map[string]interface{}{
-		"message": "Success delete data",
-	}
-	return c.JSON(http.StatusOK, res)
 }
 
 func main() {
+	db := InitDB()
 	e := echo.New()
-	e.GET("/user", GetAll)
-	e.POST("/user", InsertUser)
-	e.GET("/user/:id", GetSpecificUser)
-	e.PUT("/user/:id", UpdateUser)
-	e.DELETE("/user/:id", DeleteUser)
+	e.GET("/user", GetAll(db))
+	e.POST("/user", InsertUser(db))
+	e.GET("/user/:id", GetSpecificUser(db))
+	e.PUT("/user/:id", UpdateUser(db))
+	e.DELETE("/user/:id", DeleteUser(db))
 
 	fmt.Println("Menjalankan program ....")
 	err := e.Start(":8000")
